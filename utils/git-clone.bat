9289b5f55498b04498faec708a22ec306ce35633
Kitano <andre.kitano.silva@gmail.com>
2020-02-29 14:56:34 -0300

    Added new project with responsibility to the user

--- utils/git-clone.bat
+++ utils/git-clone.bat
@@ -1,6 +1,7 @@
 IF EXIST "%ProgramFiles%\Git" (
 	git clone https://github.com/andrekkss/conectados-middleware.git middleware
-	git clone https://github.com/andrekkss/conectados-api.git post-service
+	git clone https://github.com/andrekkss/conectados-posts.git post-service
+	git clone https://github.com/andrekkss/conectados-client.git client-service
 ) ELSE (
 	echo Por favor instale git.
 	echo Link de download: https://git-scm.com/downloads
