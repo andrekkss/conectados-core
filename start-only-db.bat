8c5e9485576ca34b63fc3d7db90ab3ad11545bb8
Andre <andre.kitano.silva@gmail.com>
2020-11-17 10:43:16 -0300

    added mob to git clone script

--- start-only-db.bat
+++
@@ -1,8 +0,0 @@
-CALL docker --version
-IF %ERRORLEVEL% == 0 (
-   CALL docker-compose --file ./docker-mongodb.yml up
-	pause
-) ELSE (
-    echo "docker nao existe"
-	pause
-)
--- utils/git-clone.bat
+++ utils/git-clone.bat
@@ -2,6 +2,7 @@ IF EXIST "%ProgramFiles%\Git" (
 	git clone https://github.com/andrekkss/conectados-middleware.git middleware
 	git clone https://github.com/andrekkss/conectados-posts.git post-service
 	git clone https://github.com/andrekkss/conectados-client.git client-service
+	git clone https://github.com/andrekkss/conectados_mob conectados-mob
 ) ELSE (
 	echo Por favor instale git.
 	echo Link de download: https://git-scm.com/downloads
